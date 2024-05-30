b67ed4f4d35896b0f92b1cd8816e16fab92968d3
IgorTkachenko <igortkachenko987@gmail.com>
2024-05-31 00:18:20 +0200

    next

--- src/String1/withoutX.java
+++ src/String1/withoutX.java
@@ -1,7 +1,8 @@
 package String1;
 
 public class withoutX {
-    //Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
+    //Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return
+    // the string unchanged.
     //withoutX("xHix") → "Hi"
     //withoutX("xHi") → "Hi"
     //withoutX("Hxix") → "Hxi"
